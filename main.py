f87f74dfce489073d23cede64594743397eaa42f
Q2sama <huangqianyi666@gmail.com>
2024-12-17 22:56:26 +0800

    filled the code

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 
-import modules.photo_hashing as photo_hashing
+import modules.excel_hashing as excel_hashing
+import pandas as pd
+import tkinter as tk
+from tkinter import ttk
+
+def create_gui_for_column_selection(columns):
+    def on_submit():
+        selected_columns.set([columns[i] for i in range(len(columns)) if var_list[i].get()])
+        root.quit()
+
+    root = tk.Tk()
+    root.title("选择需要使用的列")
+
+    label = tk.Label(root, text="请选择需要使用的列：")
+    label.pack()
+
+    var_list = []
+    for col in columns:
+        var = tk.BooleanVar()
+        chk = tk.Checkbutton(root, text=col, variable=var)
+        chk.pack(anchor='w')
+        var_list.append(var)
+
+    submit_button = tk.Button(root, text="提交", command=on_submit)
+    submit_button.pack()
+
+    selected_columns = tk.Variable()
+    root.wait_variable(selected_columns)
+    root.destroy()
+    return list(selected_columns.get())
 
 if __name__ == "__main__":
-    image_path = "receipt.png"
-    text=photo_hashing.process_image(image_path)
+    excelfile="demo/文件1734435028772.xlsx"
+    examlist=excel_hashing.to_csv(excelfile)
+    print(examlist)
+    columnlist=examlist.columns.values.tolist()
+    quest=create_gui_for_column_selection(columnlist)
+    print(list(quest))
+    quest_column=["课程名称","考试时间","考试地点","考试座号"]
--- modules/excel_hashing.py
+++ modules/excel_hashing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 def to_csv(excelfile):
     excel=pd.read_excel(excelfile)
-    excel.to_csv('output.csv',index=False)
-excelfile="./demo/文件1734435028772.xlsx"
-to_csv(excelfile)
+    excel.to_csv('./output/output.csv',index=False)
+    return pd.read_csv('./output/output.csv')
